f91bc3413ec3b8673b3bad6a9a80851e531741c8
Student <student@b426-19.intranet.cs.upt.ro>
2026-04-02 13:04:50 +0300

    added files

---
+++ src/main/java/loose/oose/fis/lab/student/manager/model/Student.java
@@ -0,0 +1,38 @@
+package loose.oose.fis.lab.student.manager.model;
+
+import java.util.Objects;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+
+    public Student(String firstName, String lastName, int age, double meanGrade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.meanGrade = meanGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + firstName + ", " + lastName + ", " + age + ", " + meanGrade + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age &&
+                Double.compare(student.meanGrade, meanGrade) == 0 &&
+                Objects.equals(firstName, student.firstName) &&
+                Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
